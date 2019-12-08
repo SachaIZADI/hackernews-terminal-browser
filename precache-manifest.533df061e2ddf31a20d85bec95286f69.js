a9bf9460685b2a5f69fe8151a4c14cdfa014fea4
SachaIZADI <sacha.izadi@polytechnique.edu>
2019-12-09 00:17:49 +0100

    Updates

---
+++ precache-manifest.533df061e2ddf31a20d85bec95286f69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895e64d222a083ea80086c640f8a9521",
+    "url": "/hackernews-terminal-browser/index.html"
+  },
+  {
+    "revision": "17cfb0599c97f186fb1e",
+    "url": "/hackernews-terminal-browser/static/js/2.1320fc24.chunk.js"
+  },
+  {
+    "revision": "3f007170c883e3473717adb3ff184152",
+    "url": "/hackernews-terminal-browser/static/js/2.1320fc24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73a37e8e8e7f33d6024e",
+    "url": "/hackernews-terminal-browser/static/js/main.378d3ed0.chunk.js"
+  },
+  {
+    "revision": "da2f9db0054466589c3c",
+    "url": "/hackernews-terminal-browser/static/js/runtime-main.cb923240.js"
+  }
+]);
