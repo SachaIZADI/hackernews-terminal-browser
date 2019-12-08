f2acdd31f2a3f227451fe7e4474162e15c88ecce
SachaIZADI <sacha.izadi@polytechnique.edu>
2019-12-09 00:13:22 +0100

    finished the terminal interface

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import Terminal from 'terminal-in-react';
 
 class App extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-
-    showMsg = () => 'Hello World'
-
     getTopStoriesIds = () => {
         return (
             fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
@@ -29,12 +22,12 @@ class App extends Component {
         )
     }
 
-    getStoriesContent = () => {
+    getStoriesContent = (maxStories) => {
         return (
             this.getTopStoriesIds()
             .then(topStoriesIds => {
                 var promiseArray = []
-                for(var i = 0; i < Math.min(topStoriesIds.length,50); i++){
+                for(var i = 0; i < Math.min(topStoriesIds.length, maxStories); i++){
                     promiseArray.push(this.getStoryContent(topStoriesIds[i]))
                 }
                 return Promise.all(promiseArray)
@@ -42,6 +35,7 @@ class App extends Component {
         )
     }
 
+    isInteger = (value) => /^\d+$/.test(value)
 
     render() {
 
@@ -51,30 +45,41 @@ class App extends Component {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                height: "100vh"
+                height: "100vh",
                 }}
             >
             <Terminal
-            color='green'
-            backgroundColor='black'
+            actionHandlers={{handleClose: (toggleClose) => {}}}
+            msg="Bash-like UI to get latest HN top stories. Type `help` if you're lost"
+            color='#93a1a1'
+            backgroundColor='#073642'
+            prompt='#93a1a1'
             barColor='black'
-            style={{ fontWeight: "bold", fontSize: "1em" }}
+            style={{ fontWeight: "bold", fontSize: "1.2em" }}
+            promptSymbol="👨‍💻>"
+            minimise={true}
+            maximise={false}
             commands={{
-                'open-google': () => window.open('https://www.google.com/', '_blank'),
-                showmsg: this.showMsg,
-                popup: () => alert('Terminal in React'),
-                'a': () => {
-                    this.getStoriesContent()
-                    .then(x => console.log(x))
-                }
+                news : (args, print, runCommand) => {
+                    const maxStories = (args.slice(1).length > 0 && this.isInteger(args.slice(1))) ? parseInt(args.slice(1)[0]) : 5
+                    this.getStoriesContent(maxStories)
+                    .then(storiesContent => {
+                        for(var i = 0; i < storiesContent.length; i++){
+                            print('----------------------')
+                            print(`${i}: ${storiesContent[i].title}`)
+                            print(`${storiesContent[i].url}`)
+                        }
+                        print('----------------------')
+                    })
+                },
+                'open-github': () => window.open('https://github.com/SachaIZADI/hackernews-terminal-browser', '_blank'),
+                'open-hn': () => window.open('https://news.ycombinator.com/', '_blank'),
             }}
             descriptions={{
-                'open-google': 'opens google.com',
-                showmsg: 'shows a message',
-                alert: 'alert', popup: 'alert',
-                'hello': 'hello'
+                news: 'shows the latest HN news',
+                'open-github': 'opens a new tab with the source code',
+                'open-hn': 'opens a new tab on HackerNews'
             }}
-            msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
             />
         </div>
     );
