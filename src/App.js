8fce0fcc920f80c73d5374df4ce00716897483fa
SachaIZADI <sacha.izadi@polytechnique.edu>
2019-12-08 21:50:09 +0100

    implementing client-side http requests to get HN content

--- src/App.js
+++ src/App.js
@@ -2,36 +2,72 @@ import React, { Component } from 'react';
 import Terminal from 'terminal-in-react';
 
 class App extends Component {
-  showMsg = () => 'Hello World'
 
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh"
-        }}
-      >
-        <Terminal
-          color='green'
-          backgroundColor='black'
-          barColor='black'
-          style={{ fontWeight: "bold", fontSize: "1em" }}
-          commands={{
-            'open-google': () => window.open('https://www.google.com/', '_blank'),
-            showmsg: this.showMsg,
-            popup: () => alert('Terminal in React')
-          }}
-          descriptions={{
-            'open-google': 'opens google.com',
-            showmsg: 'shows a message',
-            alert: 'alert', popup: 'alert'
-          }}
-          msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
-        />
-      </div>
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    showMsg = () => 'Hello World'
+
+    getTopStoriesIds = () => {
+        return (
+            fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+            .then(res => res.json())
+        )
+    }
+
+    getStoryContent = (storyId) => {
+        return (
+            fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+            .then(res => res.json())
+            .then(data => ({
+                title : data.title,
+                type : data.type,
+                url : data.url
+            }))
+        )
+    }
+
+    render() {
+        this.getTopStoriesIds()
+        .then(storyIds => {
+            this.getStoryContent(storyIds[0])
+            .then(data => console.log(data))
+        })
+
+
+        return (
+            <div
+                style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh"
+                }}
+            >
+            <Terminal
+            color='green'
+            backgroundColor='black'
+            barColor='black'
+            style={{ fontWeight: "bold", fontSize: "1em" }}
+            commands={{
+                'open-google': () => window.open('https://www.google.com/', '_blank'),
+                showmsg: this.showMsg,
+                popup: () => alert('Terminal in React'),
+                'a': () => {
+                    this.getTopStoriesIds().then(data=>console.log(data))
+                }
+            }}
+            descriptions={{
+                'open-google': 'opens google.com',
+                showmsg: 'shows a message',
+                alert: 'alert', popup: 'alert',
+                'hello': 'hello'
+            }}
+            msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+            />
+        </div>
     );
   }
 }
